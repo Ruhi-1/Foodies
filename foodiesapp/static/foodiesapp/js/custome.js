bcc23dc5e3add28362634f4bd9da35922159646a
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-09-11 18:16:22 -0500

    first commit

---
+++ foodiesapp/static/foodiesapp/js/custome.js
@@ -0,0 +1,72 @@
+
+// google map
+var map = '';
+var center;
+
+function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(13.758468, 100.567481),
+      scrollwheel: false
+    };
+  
+    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
+    });
+  
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+}
+
+function calculateCenter() {
+  center = map.getCenter();
+}
+
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
+
+//Google map
+$(function(){
+  $('.flexslider').flexslider({
+    controlNav : false,
+    nextText : '',
+    prevText : '',
+    });
+  loadGoogleMap();
+});
+
+
+$(document).on('click', '.dropdown-menu', function (e) {
+  e.stopPropagation();
+});
+
+// make it as accordion for smaller screens
+// if ($(window).width() < 992) {
+//   $('.dropdown-menu a').click(function(e){
+//     e.preventDefault();
+//       if($(this).next('.submenu').length){
+//         $(this).next('.submenu').toggle();
+//       }
+//       $('.dropdown').on('hide.bs.dropdown ', function () {
+//       $(this).find('.submenu').open();
+//   })
+//   });
+// }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
