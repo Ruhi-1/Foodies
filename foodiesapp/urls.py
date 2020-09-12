4384df002846578ffc0ec9c9db4d87e5a73922b8
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-09-11 19:10:10 -0500

    added breakfast.html page and lunch.html page

--- foodiesapp/urls.py
+++ foodiesapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('breakfast/', views.breakfast, name='breakfast'),
+    path('lunch/', views.lunch, name='lunch'),
+
 ]
--- foodiesapp/views.py
+++ foodiesapp/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'foodiesapp/index.html')
+
+def breakfast(request):
+    return render(request, 'foodiesapp/breakfast.html')
+
+def lunch(request):
+    return render(request, 'foodiesapp/lunch.html')
