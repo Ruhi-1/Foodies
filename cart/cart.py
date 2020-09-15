11c85b2099c12cd94988e72b7e3ab2233e4e0c86
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-09-15 15:50:18 -0500

    added save and remove function

--- cart/cart.py
+++ cart/cart.py
@@ -27,3 +27,19 @@ def add(self, product, quantity=1, update_quantity=False):
         else:
             self.cart[product_id]['quantity'] += quantity
         self.save()
+
+def save(self):
+    # update the session cart
+    self.session[settings.CART_SESSION_ID] = self.Cart
+    # mark the session as 'modified' to make sure it is saved
+    self.session.modified = True
+
+def remove(self, product):
+    """
+    Remove a product from the cart.
+    """
+    product_id = str(product.id)
+    if product_id in self.cart:
+        del self.cart[product_id]
+        self.save()
+
