acfcce1975029bb8aa5e42d4e9ec6ce59e5b7684
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-09-19 19:27:39 -0500

    menu links working

--- foodiesapp/static/foodiesapp/js/custom.js
+++ foodiesapp/static/foodiesapp/js/custom.js
@@ -50,7 +50,7 @@ $(document).on('click', '.dropdown-menu', function (e) {
 // make it as accordion for smaller screens
 if ($(window).width() < 992) {
   $('.dropdown-menu a').click(function(e){
-    e.preventDefault();
+    // e.preventDefault();
       if($(this).next('.submenu').length){
         $(this).next('.submenu').toggle();
       }
