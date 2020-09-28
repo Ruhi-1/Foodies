c07ed461e768bceb044793d00dd58f5a8f4016a1
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-09-28 18:11:17 -0500

    added add to cart button on burgers.html page

--- foodies1/settings.py
+++ foodies1/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'foodiesapp',
+    'cart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -60,6 +61,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                # 'cart.context_processor.cart_total_amount'
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
--- foodiesapp/models.py
+++ foodiesapp/models.py
@@ -1,33 +1,37 @@
 from django.db import models
 
+
+
+
+
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+# class Category(models.Model):
+#     name = models.CharField(max_length=200, db_index=True)
+#     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
+#     class Meta:
+#         ordering = ('name',)
+#         verbose_name = 'category'
+#         verbose_name_plural = 'categories'
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    # image = models.SlugField(upload_to='products/YY/MM/DD', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+# class Product(models.Model):
+#     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+#     name = models.CharField(max_length=200, db_index=True)
+#     slug = models.SlugField(max_length=200, db_index=True)
+#     # image = models.SlugField(upload_to='products/YY/MM/DD', blank=True)
+#     description = models.TextField(blank=True)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     stock = models.PositiveIntegerField()
+#     available = models.BooleanField(default=True)
+#     created = models.DateTimeField(auto_now_add=True)
+#     updated = models.DateTimeField(auto_now=True)
 
-class Meta:
-    ordering = ('-created',)
-    index_together = (('id', 'slug'),)
+# class Meta:
+#     ordering = ('-created',)
+#     index_together = (('id', 'slug'),)
 
-def __str__(self):
-    return self.name
+# def __str__(self):
+#     return self.name
